7bf938d4079943262cd973cb84a9fde278380c36
Mark Breen <markvader@users.noreply.github.com>
2022-11-07 00:59:29 +0000

    strip back the code to try and debug connection issues
    Same credetials are working through Postman app.
    Is my issue something to do with ssl_context.verify_mode or have I another issue?

---
+++ sonic_local.py
@@ -0,0 +1,47 @@
+import asyncio
+import base64
+import websockets
+import json
+import ssl
+import logging
+
+logger = logging.getLogger('websockets')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+# USER TO PROVIDE FOLLOWING VARIABLES
+username = "ENTERusernameFROMsignalSETTINGSinTHEsonicAPP"
+password = "ENTERpasswordFROMsignalSETTINGSinTHEsonicAPP"
+ip_address = "find your ip address on your local network"
+
+
+port = 443
+uri = f"wss://{ip_address}:{port}"
+
+
+async def sonic_connect():
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    # ssl_context.create_default_https_context = ssl._create_unverified_context
+
+    user_pass = f"{username}:{password}"
+    headers = {'Authorization': f"Basic {base64.b64encode(user_pass.encode()).decode()}"}
+    # Following headers already included in websockets.connect(), therefore not needed
+    # headers['Connection'] = 'Upgrade'
+    # headers['Upgrade'] = 'websocket'
+
+    print(f"{uri}")
+    print(f"{headers}")
+    print(f"{ssl_context}")
+
+    transmit_msg = json.dumps({"event": "requestTelemetry"})
+    print(f"{transmit_msg}")
+
+    async with websockets.connect(uri, extra_headers=headers, ssl=ssl_context) as websocket:
+        await websocket.send(json.dumps({"event": "requestTelemetry"}))
+        response = await websocket.recv()
+        print(f"{response}")
+
+if __name__ == "__main__":
+    asyncio.run(sonic_connect())
