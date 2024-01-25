2071ef62b2cdd54d6cf47517ff24c04b63e4e326
Mark Breen <markvader@users.noreply.github.com>
2024-01-25 22:58:47 +0000

    tidy up parser.add_arguments

--- sonic_local/__main__.py
+++ sonic_local/__main__.py
@@ -19,30 +19,11 @@ async def main(options):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--ip_address",
-        "-i",
-        required=True,
-        help="IP address of the device e.g 192.168.1.10"
-    )
-    parser.add_argument(
-        "--username",
-        "-u",
-        required=True,
-        help="Username for the device"
-    )
-    parser.add_argument(
-        "--password",
-        "-p",
-        required=True,
-        help="Password for the device"
-    )
-    parser.add_argument(
-        "--command",
-        "-c",
-        required=True,
-        help="Command to send to the device, e.g. requestState, requestTelemetry, open, closed"
-    )
+    parser.add_argument("-i", "--ip_address", required=True, help="IP address of the device e.g 192.168.1.10")
+    parser.add_argument("-u", "--username", required=True, help="Username for the device")
+    parser.add_argument("-p", "--password", required=True, help="Password for the device")
+    parser.add_argument("-c", "--command",  required=True, default="requestTelemetry",
+                        help="Command to send to the device, e.g. requestState, requestTelemetry, open, closed")
     args = parser.parse_args()
 
     loop = asyncio.get_event_loop()
